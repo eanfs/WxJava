6285efdc7e3550401c677acae78e4187e369387a
richard.li <richard.li@esenyun.com>
2018-11-20 19:36:14 +0800

    Update for wxcp add suite api

--- weixin-java-cp/src/main/java/me/chanjar/weixin/cp/config/impl/WxCpDefaultConfigImpl.java
+++ weixin-java-cp/src/main/java/me/chanjar/weixin/cp/config/impl/WxCpDefaultConfigImpl.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.util.Hashtable;
 import java.util.Map;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import me.chanjar.weixin.common.bean.WxAccessToken;
 import me.chanjar.weixin.common.util.http.apache.ApacheHttpClientBuilder;
 import me.chanjar.weixin.cp.config.WxCpConfigStorage;
@@ -24,6 +27,9 @@ public class WxCpDefaultConfigImpl implements WxCpConfigStorage, Serializable {
 
   private volatile String corpId;
   private volatile String corpSecret;
+  protected volatile Map<String, String> suiteVerifyTickets = new Hashtable<>();
+  protected volatile Map<String, String> suiteAccessTokens = new Hashtable<>();
+
   protected volatile Map<String, String> suiteVerifyTickets = new Hashtable<>();
   protected volatile Map<String, String> suiteAccessTokens = new Hashtable<>();
 
@@ -66,6 +72,26 @@ public class WxCpDefaultConfigImpl implements WxCpConfigStorage, Serializable {
       baseApiUrl = WxCpApiPathConsts.DEFAULT_CP_BASE_URL;
     }
     return baseApiUrl + path;
+  }
+
+  @Override
+  public String getSuiteVerifyTicket(String suiteId) {
+    return suiteVerifyTickets.get(suiteId);
+  }
+
+  @Override
+  public String getSuiteAccessToken(String suiteId) {
+    return suiteAccessTokens.get(suiteId);
+  }
+
+  @Override
+  public void updateSuiteVerifyTicket(String suiteId, String ticket, int expiresIn) {
+    suiteVerifyTickets.put(suiteId, ticket);
+  }
+
+  @Override
+  public void updateSuiteAccessToken(String authCorpId, String accessToken, int expiresIn) {
+    suiteAccessTokens.put(authCorpId, accessToken);
   }
 
   @Override
